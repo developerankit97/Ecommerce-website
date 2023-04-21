9d5f442e22fc07d00bb248a5bf6a8ff90abf2f84
developerankit97 <ankitsh.developer@gmail.com>
2023-04-21 16:19:37 +0530

    router added

---
+++ routes/shop.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.send("<h1>This is a HomePage</h1>");
+})
+
+module.exports = router;
