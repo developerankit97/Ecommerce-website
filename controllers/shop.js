7568032d3da21a3467f032a4ae6df954770da9f3
developerankit97 <ankitsh.developer@gmail.com>
2023-05-09 11:33:28 +0530

    orders functionality completed

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => {
 	Product.find()
@@ -75,8 +76,7 @@ exports.postCart = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-	req.user
-		.getOrders()
+	Order.find({ 'users.userId': req.user._id })
 		.then((orders) => {
 			res.render('shop/orders', {
 				path: '/orders',
@@ -89,10 +89,27 @@ exports.getOrders = (req, res, next) => {
 
 exports.postOrder = (req, res, next) => {
 	req.user
-		.addOrder()
-		.then((result) => {
-			res.redirect('/orders');
+		.populate('cart.items.productId')
+		.then((user) => {
+			const products = user.cart.items.map((i) => {
+				return {
+					quantity: i.quantity,
+					product: { ...i.productId._doc },
+				};
+			});
+			const order = new Order({
+				users: {
+					name: req.user.name,
+					userId: req.user._id,
+				},
+				products: products,
+			});
+			return order.save();
 		})
+		.then((result) => {
+			return req.user.clearCart();
+		})
+		.then(() => res.redirect('/orders'))
 		.catch((err) => console.log(err));
 };
 
